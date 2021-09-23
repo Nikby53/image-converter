ffdf88fe6a0a3e5792a1d130d2621d2d7370cc55
Nikby53 <nikdiablo3@gmail.com>
2021-09-24 00:25:40 +0300

    Add converting business logic

--- internal/service/images.go
+++ internal/service/images.go
@@ -1,7 +1,42 @@
 package service
 
-import "github.com/Nikby53/image-converter/internal/models"
+import (
+	"bytes"
+	"errors"
+	"image"
+	"image/jpeg"
+	"image/png"
+
+	"github.com/Nikby53/image-converter/internal/models"
+)
 
 func (s *Service) UploadImage(image models.Images) (string, error) {
 	return s.repoImage.UploadImage(image)
+}
+
+const (
+	JPG  = "jpg"
+	PNG  = "png"
+	JPEG = "jpeg"
+)
+
+func (s *Service) Convert(imageBytes []byte, targetFormat string) ([]byte, error) {
+	img, _, err := image.Decode(bytes.NewReader(imageBytes))
+	if err != nil {
+		return nil, errors.New("unable to decode image")
+	}
+	buf := new(bytes.Buffer)
+	switch targetFormat {
+	case PNG:
+		var enc png.Encoder
+		err := enc.Encode(buf, img)
+		if err != nil {
+			return nil, errors.New("can't convert in jpg")
+		}
+	case JPG, JPEG:
+		if err := jpeg.Encode(buf, img, &jpeg.Options{Quality: 1}); err != nil {
+			return nil, errors.New("can't convert in png")
+		}
+	}
+	return buf.Bytes(), nil
 }
