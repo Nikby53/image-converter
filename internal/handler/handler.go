fb8ca1d652a75a164f392b841b26926b370ae18f
Nikby53 <nikdiablo3@gmail.com>
2021-09-24 00:23:21 +0300

    Rename endpoint and use interface instead of service struct in server

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -9,20 +9,21 @@ import (
 
 type Server struct {
 	router   *mux.Router
-	services service.Authorization
+	services service.ServiceInterface
 }
 
-func NewServer(service service.Authorization) *Server {
+func NewServer(service service.ServiceInterface) *Server {
 	s := Server{
 		router:   mux.NewRouter(),
 		services: service,
 	}
 	s.router.HandleFunc("/sign-up", s.signUp).Methods("POST")
-	s.router.HandleFunc("/sign-in", s.signIn).Methods("POST")
+	s.router.HandleFunc("/login", s.login).Methods("POST")
 	api := s.router.NewRoute().Subrouter()
 	api.Use(s.UserIdentity)
 	api.HandleFunc("/requestHistory", s.requestHistory).Methods("GET")
 	api.HandleFunc("/convert", s.convert).Methods("POST")
+
 	return &s
 }
 
