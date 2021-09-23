bf7fc7f10e2f6d3c822444e43c07f20d89bf9495
Nikby53 <nikdiablo3@gmail.com>
2021-09-24 00:24:48 +0300

    Add converting implementation in convert handler

--- internal/handler/image.go
+++ internal/handler/image.go
@@ -1,9 +1,33 @@
 package handler
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
 
 func (s *Server) convert(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		logrus.Printf("error retrieving the File %v", err)
+		return
+	}
 
+	defer file.Close()
+	sourceFormat := r.FormValue("sourceFormat")
+	targetFormat := r.FormValue("targetFormat")
+	filename := strings.TrimSuffix(header.Filename, "."+sourceFormat)
+	fileBytes, _ := ioutil.ReadAll(file)
+	convImageBytes, err := s.services.Convert(fileBytes, targetFormat)
+	if err != nil {
+		logrus.Printf("can't convert image: %v", err)
+		return
+	}
+	ioutil.WriteFile(filename+"."+targetFormat, convImageBytes, 0644)
+	fmt.Fprintf(w, "successfully uploaded file\n")
 }
 
 func (s *Server) requestHistory(w http.ResponseWriter, r *http.Request) {
