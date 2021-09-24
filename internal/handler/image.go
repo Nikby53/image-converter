e671dc31d5f6c34ba6fd925bbeba0196ca8da67e
nikby53 <nikdiablo3@gmail.com>
2021-09-24 15:31:32 +0300

    Add validate func for convert handler

--- internal/handler/image.go
+++ internal/handler/image.go
@@ -9,6 +9,16 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+func ValidateConvert(sourceFormat, filename, targetFormat string) error {
+	if len(filename) == 0 {
+		return fmt.Errorf("name of the file should not be empty")
+	}
+	if len(sourceFormat) == 0 {
+		return fmt.Errorf("name of the format should not be empty")
+	}
+	return nil
+}
+
 func (s *Server) convert(w http.ResponseWriter, r *http.Request) {
 	file, header, err := r.FormFile("file")
 	if err != nil {
@@ -21,12 +31,20 @@ func (s *Server) convert(w http.ResponseWriter, r *http.Request) {
 	targetFormat := r.FormValue("targetFormat")
 	filename := strings.TrimSuffix(header.Filename, "."+sourceFormat)
 	fileBytes, _ := ioutil.ReadAll(file)
+	err = ValidateConvert(sourceFormat, filename, targetFormat)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 	convImageBytes, err := s.services.Convert(fileBytes, targetFormat)
 	if err != nil {
 		logrus.Printf("can't convert image: %v", err)
 		return
 	}
-	ioutil.WriteFile(filename+"."+targetFormat, convImageBytes, 0644)
+	err = ioutil.WriteFile(filename+"."+targetFormat, convImageBytes, 0644)
+	if err != nil {
+		return
+	}
 	fmt.Fprintf(w, "successfully uploaded file\n")
 }
 
