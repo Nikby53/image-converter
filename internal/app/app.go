9bdedab396376137afe2901f838d4e511d78031c
Nikby53 <nikdiablo3@gmail.com>
2021-09-26 20:08:03 +0300

    Set config in app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,12 +2,12 @@ package app
 
 import (
 	"net/http"
-	"os"
+
+	"github.com/Nikby53/image-converter/internal/storage"
 
 	"github.com/Nikby53/image-converter/internal/handler"
 	"github.com/Nikby53/image-converter/internal/repository"
 	"github.com/Nikby53/image-converter/internal/service"
-	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -17,16 +17,13 @@ func Run() error {
 	if err := initConfig(); err != nil {
 		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
-	if err := godotenv.Load(); err != nil {
-		logrus.Fatalf("error loading env variables: %s", err.Error())
-	}
 	db, err := repository.NewPostgresDB(repository.Config{
 		Host:     viper.GetString("db.host"),
 		Port:     viper.GetString("db.port"),
 		Username: viper.GetString("db.username"),
 		DBName:   viper.GetString("db.dbname"),
 		SSLMode:  viper.GetString("db.sslmode"),
-		Password: os.Getenv("DB_PASSWORD"),
+		Password: viper.GetString("db.password"),
 	})
 	if err != nil {
 		logrus.Fatalf("failed to initialize db: %s", err.Error())
@@ -34,7 +31,16 @@ func Run() error {
 	repos := repository.New(db)
 	reposImage := repository.New(db)
 	services := service.New(repos, reposImage)
-	srv := handler.NewServer(services)
+	st, err := storage.New(storage.Config{
+		BucketName: viper.GetString("awsS3.bucketName"),
+		AccId:      viper.GetString("awsS3.accId"),
+		SecretKey:  viper.GetString("awsS3.secretKey"),
+		Region:     viper.GetString("awsS3.region"),
+	})
+	if err != nil {
+		logrus.Fatalf("failed to initialize awsS3 storage: %s", err.Error())
+	}
+	srv := handler.NewServer(services, st)
 
 	if err := http.ListenAndServe(viper.GetString("port"), srv); err != nil && err != http.ErrServerClosed {
 		logrus.Printf("ListenAndServe(): %s", err)
@@ -43,7 +49,7 @@ func Run() error {
 }
 
 func initConfig() error {
-	viper.AddConfigPath("internal/configs")
+	viper.AddConfigPath("./")
 	viper.SetConfigName("config")
 	return viper.ReadInConfig()
 }
