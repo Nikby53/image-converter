a3d6062a9de6fe9c2b674a997f99638859356fe6
Nikby53 <nikdiablo3@gmail.com>
2021-10-03 20:26:00 +0300

    Update errors and refactor get user func

--- internal/repository/auth_postgres.go
+++ internal/repository/auth_postgres.go
@@ -12,7 +12,7 @@ func (r *Repository) CreateUser(user models.User) (int, error) {
 	query := fmt.Sprintf("INSERT INTO %s (email, password) values ($1, $2) RETURNING id", users)
 	row := r.db.QueryRow(query, user.Email, user.Password)
 	if err := row.Scan(&id); err != nil {
-		return 0, err
+		return 0, fmt.Errorf("cannot create user: %w", err)
 	}
 	return id, nil
 }
@@ -21,6 +21,9 @@ func (r *Repository) CreateUser(user models.User) (int, error) {
 func (r *Repository) GetUser(email, password string) (models.User, error) {
 	var user models.User
 	query := fmt.Sprintf("SELECT id FROM %s WHERE email=$1 AND password=$2", users)
-	err := r.db.Get(&user, query, email, password)
-	return user, err
+	row := r.db.QueryRow(query, email, password)
+	if err := row.Scan(&user.Id); err != nil {
+		return models.User{}, fmt.Errorf("cannot find the user in database:%w", err)
+	}
+	return user, nil
 }
