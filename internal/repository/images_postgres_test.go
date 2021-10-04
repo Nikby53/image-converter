953bc0bbf957ebf5e2e99f76fb6fe7dc60c579f6
Nikby53 <nikdiablo3@gmail.com>
2021-10-05 02:10:06 +0300

    Add error test case in insert image and implement request history test

--- internal/repository/images_postgres_test.go
+++ internal/repository/images_postgres_test.go
@@ -12,39 +12,111 @@ func TestRepository_InsertImage(t *testing.T) {
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
+	type args struct {
+		filename string
+		format   string
+	}
+	query := "INSERT INTO images"
 	defer db.Close()
 	r := New(db)
 	tests := []struct {
-		name          string
-		mock          func()
-		filenameInput string
-		formatInput   string
-		want          string
-		wantErr       bool
+		name    string
+		mock    func()
+		input   args
+		want    string
+		wantErr bool
 	}{
 		{
 			name: "Ok",
 			mock: func() {
 				rows := sqlmock.NewRows([]string{"id"}).AddRow(1)
-				mock.ExpectQuery("INSERT INTO images").
+				mock.ExpectQuery(query).
 					WithArgs("image", "jpg").WillReturnRows(rows)
 			},
-			filenameInput: "image",
-			formatInput:   "jpg",
-			want:          "1",
+			input: args{
+				filename: "image",
+				format:   "jpg",
+			},
+			want:    "1",
+			wantErr: false,
 		},
 		{
-			name: "Error",
+			name: "Empty Fields error",
 			mock: func() {
-
+				rows := sqlmock.NewRows([]string{"id"})
+				mock.ExpectQuery(query).
+					WithArgs("image", "").WillReturnRows(rows)
 			},
+			input: args{
+				filename: "image",
+				format:   "",
+			},
+			wantErr: true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.mock()
-			got, err := r.InsertImage(tt.filenameInput, tt.formatInput)
+			got, err := r.InsertImage(tt.input.filename, tt.input.format)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want, got)
+			}
+			assert.NoError(t, mock.ExpectationsWereMet())
+		})
+	}
+}
+
+func TestRepository_RequestsHistory(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	type args struct {
+		sourceFormat string
+		targetFormat string
+		imagesId     string
+		filename     string
+		userId       int
+		ratio        int
+	}
+	query := "INSERT INTO request"
+	defer db.Close()
+	r := New(db)
+	tests := []struct {
+		name    string
+		mock    func()
+		input   args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "Ok",
+			mock: func() {
+				rows := sqlmock.NewRows([]string{"id"}).AddRow(1)
+				mock.ExpectQuery(query).
+					WithArgs("jpg", "png", "1", "image", 1, 54).WillReturnRows(rows)
+			},
+			input: args{
+				sourceFormat: "jpg",
+				targetFormat: "png",
+				imagesId:     "1",
+				filename:     "image",
+				userId:       1,
+				ratio:        54,
+			},
+			want:    "1",
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mock()
+			got, err := r.RequestsHistory(tt.input.sourceFormat, tt.input.targetFormat, tt.input.imagesId, tt.input.filename, tt.input.userId, tt.input.ratio)
 			if tt.wantErr {
 				assert.Error(t, err)
 			} else {
